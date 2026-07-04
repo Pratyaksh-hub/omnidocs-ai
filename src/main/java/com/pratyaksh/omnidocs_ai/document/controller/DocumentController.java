ddd640a9916aeabb7ad9566b7c31e9b4d4453fb6
Pratyaksh-hub <pratyakshm111@gmail.com>
2026-07-04 17:09:31 +0530

    fixing CORS error

--- src/main/java/com/pratyaksh/omnidocs_ai/document/controller/DocumentController.java
+++ src/main/java/com/pratyaksh/omnidocs_ai/document/controller/DocumentController.java
@@ -25,7 +25,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -42,7 +41,6 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/v1/documents")
 @RequiredArgsConstructor
 @Validated
-@CrossOrigin("*")
 public class DocumentController {
 
   private static final long MAX_FILE_SIZE = 20 * 1024 * 1024;
--- src/main/java/com/pratyaksh/omnidocs_ai/workspace/controller/WorkspaceController.java
+++ src/main/java/com/pratyaksh/omnidocs_ai/workspace/controller/WorkspaceController.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -29,7 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/workspaces")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class WorkspaceController {
 
   private final WorkspaceFacade workspaceFacade;
